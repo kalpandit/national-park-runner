9c03ac7daa79f5373672eefe0e790e82efaf3aa4
Dev Rijhwani <dhr41@scarletmail.rutgers.edu>
2025-02-01 15:14:37 -0500

    testing

--- backend/src/models/itinerary_object.py
+++ backend/src/models/itinerary_object.py
@@ -1,12 +1,13 @@
 #testing
 class itinerary_object:
 
-    def __init__(self, activity, start_time, tags = {}):
+    def __init__(self, activity, start_time, rating, tags = {}):
 
 
         self.start_time = start_time
         self.end_time = self.tags.get("length", 12)+start_time
         self.activity = activity
+        self.rating=rating
         self.tags = tags
 
     
