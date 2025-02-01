2b9c1c5867c949b6152782a01ffe75f7cb8e2a71
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2025-02-01 12:27:54 -0500

    adding route

---
+++ national-park-ui/app/pages/itinerary_home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItineraryHome = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Itinerary Home</h1>
+      <p>Welcome to your itinerary home page! Here, you can view and manage your itineraries.</p>
+      {/* Add more content or functionality as needed */}
+    </div>
+  );
+};
+
+export default ItineraryHome;
