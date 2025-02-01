4f332fe564cbf28b4e6cdc2f95ef4e7982dc6ca1
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2025-02-01 13:44:28 -0500

    migration to json_string

---
+++ backend/src/database/migrate.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from collections import defaultdict
+import json
+
+def convert_json(file_path):
+
+    df = pd.read_csv(file_path)
+
+    national_parks = defaultdict(list)
+    for idx, value in df.iterrows():
+
+        activity = {}
+        activity['name'] = value['Name']
+        activity['type'] = value['Type']
+        activity['difficulty'] = value['Difficulty:']
+        activity['length'] = value['Length:']
+        activity['Accessible'] = value['Accessible:']
+        activity['Education'] = value['Education:']
+        activity['Rating'] = value['Rating']
+    
+        national_parks[value['National Park']].append(activity)
+
+    json_string = json.dumps(national_parks)
+    return json_string
+
+json_string = convert_json("nationalparks.csv")
