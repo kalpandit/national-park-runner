cedeab2322a4e386bf545c913822c7b9fee6d804
Dev Rijhwani <dhr41@scarletmail.rutgers.edu>
2025-02-01 17:43:15 -0500

    changes

--- backend/src/models/itinerary.py
+++ backend/src/models/itinerary.py
@@ -1,60 +1,95 @@
 from itinerary_object import itinerary_object
+from pymongo import MongoClient
 from user import user
 
 
 class itinerary:
 
-    def __init__(self):
+    def __init__(self, difficulty, cost):
 
         # A list of itinerary_objects
-        self.itinerary_objects = []
-        self.alternative_options = []
+        self.itinerary_objects_morning = []
+        self.alternative_options_morning= []
+
+        self.itinerary_objects_noon = []
+        self.alternative_options_noon= []
+
+        self.itinerary_objects_night = []
+        self.alternative_options_night= []
+
+        self.difficulty=difficulty
+        self.cost=cost
+
         self.objs_used = {}
         self.food = True
     
-    def add_object(self, itinerary_object_: itinerary_object):
+    def populate_itinerary(self, itinerary_object_: itinerary_object):
         new_itinerary = []
         self.objs_used.clear()
         inserted = False
 
-        for obj in self.itinerary_objects:
-            if obj.end_time <= itinerary_object_.start_time or obj.start_time >= itinerary_object_.end_time:
-                new_itinerary.append(obj)
-                self.objs_used[itinerary_object_.name] = True
-            else:
-                if not inserted:
-                    new_itinerary.append(itinerary_object_)
-                    self.objs_used[itinerary_object_.name] = True
-                    inserted = True
+        client = MongoClient("mongodb://localhost:27017/")  # check if this is correct
+        db = client["npr_db"]  # Replace with your database name
+        collection = db["model_collection"]  # Replace with your collection name
 
-        if not inserted:
-            new_itinerary.append(itinerary_object_)
+        
+
+
+        #find out how to querey mangodb to find the most popular activities based on the user --> if user likes medium difficulty automatically populate
+        #the mornign afternoon and night with 5 star medium difficulty ratings and if there aren't enough, then go down to easy rating
+
+        # time_filter = {} if self. == "all" else {"time_of_day": preferred_time}
+    
+
+        query = {"difficulty": self.difficulty , "time_of_day": "Morning" }  # Filter by difficulty and time of day
+        top_activities = collection.find(query).sort("Rating", -1).limit(3)  # Sort by Rating (descending)
+        top_3_morning=list(top_activities)
+        self.itinerary_objects_morning.append(top_3_morning)
+        
+        if len(top_3_morning)<3:
+            #work on this logic later
+                    
+
+        
+
+        # for obj in self.itinerary_objects:
+        #     # if obj.end_time <= itinerary_object_.start_time or obj.start_time >= itinerary_object_.end_time:
+        #     new_itinerary.append(obj)
+        #     self.objs_used[itinerary_object_.name] = True
+        #     # else:
+        #     if not inserted:
+        #         new_itinerary.append(itinerary_object_)
+        #         self.objs_used[itinerary_object_.name] = True
+        #         inserted = True
+
+        # if not inserted:
+        #     new_itinerary.append(itinerary_object_)
 
         self.itinerary_objects = new_itinerary
 
-    def remove_object(self, itinerary_object_: itinerary_object):
-        isDeleted=False
-        for i in self.itinerary_objects:
-            if i == itinerary_object_:
-                self.itinerary_objects.remove(i)
-                isDeleted=True
-                break
-        if isDeleted == True:
-            return True
-        return False
+    # def remove_object(self, itinerary_object_: itinerary_object):
+    #     isDeleted=False
+    #     for i in self.itinerary_objects:
+    #         if i == itinerary_object_:
+    #             self.itinerary_objects.remove(i)
+    #             isDeleted=True
+    #             break
+    #     if isDeleted == True:
+    #         return True
+    #     return False
     
-    def reschedule(self, itinerary_object_: itinerary_object, rescheduledTime):
-        checkValidTime = self.checkValidItineraryObj(itinerary_object_, rescheduledTime)
-        if checkValidTime == False:
-            print("The timeslot you selected is invalid. Please try again.")
-            return False
-        isRemoved = self.remove_object(itinerary_object_)
-        if isRemoved == False:
-            print("Unable to reschedule as activity doesn't exist")
-            return False
-        #itinerary_object_.end_time
-        itinerary_object_.start_time=rescheduledTime
-        self.add_object(itinerary_object_)#not sure if this accounts for the end time.
+    # def reschedule(self, itinerary_object_: itinerary_object, rescheduledTime):
+    #     checkValidTime = self.checkValidItineraryObj(itinerary_object_, rescheduledTime)
+    #     if checkValidTime == False:
+    #         print("The timeslot you selected is invalid. Please try again.")
+    #         return False
+    #     isRemoved = self.remove_object(itinerary_object_)
+    #     if isRemoved == False:
+    #         print("Unable to reschedule as activity doesn't exist")
+    #         return False
+    #     #itinerary_object_.end_time
+    #     itinerary_object_.start_time=rescheduledTime
+    #     self.add_object(itinerary_object_)#not sure if this accounts for the end time.
         
     
     def checkValidItineraryObj(self, itinerary_object_, rescheduledTime):
--- backend/src/models/itinerary_object.py
+++ backend/src/models/itinerary_object.py
@@ -1,11 +1,12 @@
 #testing
 class itinerary_object:
 
-    def __init__(self, activity, start_time, rating, tags = {}):
+    def __init__(self, activity, time_of_day, rating, tags = {}):
 
 
-        self.start_time = start_time
-        self.end_time = self.tags.get("length", 12)+start_time
+        #self.start_time = start_time
+        #self.end_time = self.tags.get("length", 12)+start_time
+        self.time_of_day=time_of_day
         self.activity = activity
         self.rating=rating
         self.tags = tags
