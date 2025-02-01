347355b9cabd679e85c6a0b41f9f8e5084479f82
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2025-02-01 12:14:33 -0500

    some additions

--- backend/src/models/itinerary.py
+++ backend/src/models/itinerary.py
@@ -9,17 +9,22 @@ class itinerary:
         # A list of itinerary_objects
         self.itinerary_objects = []
         self.alternative_options = []
+        self.objs_used = {}
+        self.food = True
     
     def add_object(self, itinerary_object_: itinerary_object):
         new_itinerary = []
+        self.objs_used.clear()
         inserted = False
 
         for obj in self.itinerary_objects:
             if obj.end_time <= itinerary_object_.start_time or obj.start_time >= itinerary_object_.end_time:
                 new_itinerary.append(obj)
+                self.objs_used[itinerary_object_.name] = True
             else:
                 if not inserted:
                     new_itinerary.append(itinerary_object_)
+                    self.objs_used[itinerary_object_.name] = True
                     inserted = True
 
         if not inserted:
@@ -29,12 +34,18 @@ class itinerary:
 
 def create_itinerary(user_: user):
 
-    itin = itinerary()
+    itinerary_ = itinerary()
     cost = user_.tags.get("cost", None)
     difficulty = user_.tags.get("difficulty", "Hard")
 
     # Select 
     options = "MONGO DB QUERY"  # sort by rating
+    activity_options = "MONGO DB QUERY" # sort by rating, include difficulty --> decrease the rating 
+    food_options = "MONGO DB QUERY" # sort by rating, include cost
 
+    for activity in activity_options:
+        itinerary_.add_object()
+
+    return itinerary_
 
 
